b8927641a1fdfcba45d794735f068ec7aa12b106
Shiva Ramezani <sh.ramezani94@gmail.com>
2024-08-26 18:28:46 +0330

    Add Course page

--- app/app.py
+++ app/app.py
@@ -4,10 +4,16 @@ from flask import render_template
 
 app = Flask(__name__)
 
-students = [
+student_list = [
     {'name': 'John Doe', 'email': 'john.doe@example.com', 'major': 'Computer Science'},
     {'name': 'Jane Smith', 'email': 'jane.smith@example.com', 'major': 'Mathematics'},
     {'name': 'Emily Johnson', 'email': 'emily.johnson@example.com', 'major': 'Physics'}
+]
+
+course_list = [
+    {'name': 'Introduction to Programming', 'description': 'Learn the basics of programming.', 'teacher': 'Dr. Smith'},
+    {'name': 'Advanced Mathematics', 'description': 'Deep dive into complex mathematical concepts.', 'teacher': 'Prof. Johnson'},
+    {'name': 'Physics Fundamentals', 'description': 'Understand the core principles of physics.', 'teacher': 'Dr. Brown'}
 ]
 
 @app.route('/')
@@ -16,11 +22,11 @@ def home():
 
 @app.route('/profile')
 def profile():
-    return render_template('profile.html', students = students)
+    return render_template('profile.html', student_list = student_list)
 
 @app.route('/courses')
 def courses():
-    return render_template('courses.html')
+    return render_template('courses.html', course_list = course_list)
 
 
 if __name__=='__main__':
